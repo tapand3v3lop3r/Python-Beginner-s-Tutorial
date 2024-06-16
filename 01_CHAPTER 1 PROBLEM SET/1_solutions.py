d3e8b49b46338800049d49ca3674b44d59431f6f
tapand3v3lop3r <164610686+tapand3v3lop3r@users.noreply.github.com>
2024-06-17 01:44:08 +0530

    1_solutions.py

--- 01_CHAPTER 1 PROBLEM SET/1_solutions.py
+++ 01_CHAPTER 1 PROBLEM SET/1_solutions.py
@@ -1,1 +1,25 @@
+1. 
+print(
+      '''
+      Twinkle twinkle little star.
+      How I wonder what you are.
+      Up above the world so high.
+      Like a diamond in the sky.
+      Twinkle twinkle little star.
+      How I wonder what you are.
+
+      Twinkle twinkle little star.
+      How I wonder what you are.
+      Up above the world so high.
+      Like a diamond in the sky.
+      Twinkle twinkle little star.
+      How I wonder what you are.
+      '''
+)
+
+# Explanation of Docstring Usage:
+# Purpose: The docstring (""" ... """) at the beginning of the script explains the purpose of the script, which is to print the "Twinkle Twinkle Little Star".
+
+
+2.
 
