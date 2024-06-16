54c6058e108945a6eea1279a7473253921e3a378
tapand3v3lop3r <164610686+tapand3v3lop3r@users.noreply.github.com>
2024-06-17 00:51:24 +0530

    welcome.py

--- CHAPTER 1/first.py
+++ CHAPTER 1/first.py
@@ -1,1 +1,35 @@
-print("hey there ....")
+# Welcome to the Python Beginners Tutorial! 🎉
+# I'm excited to have you here. Whether you're completely new to programming or looking to brush up on your skills, you've come to the right place.
+# Throughout this tutorial, I'll guide you through the basics of Python, one of the most popular and versatile programming languages.
+# By the end, you'll have a solid foundation and be ready to tackle more advanced topics.
+# Let's start our coding journey together!
+
+# Our very first Python program!
+# The print() function is used to display text on the screen.
+# It's one of the simplest but most important functions in Python.
+# Let's start by printing a friendly greeting message to the console.
+print("Hey there, welcome to your Python journey!")
+
+# Now, let's try a few variations to see different outputs and reinforce the use of the print() function.
+
+# Trying a different message
+# You can change the text inside the quotes to print different messages.
+print("Hello, world!")
+
+# Adding excitement to the message
+# Feel free to add extra punctuation or words to express your enthusiasm!
+print("Hey there, welcome to your Python journey!!!")
+
+# Printing a multi-line message
+# Using the newline character (\n) to print multiple lines in one print statement.
+print("Hey there,\nWelcome to your Python journey!\nLet's have some fun learning Python together.")
+
+# Including a quote in the message
+# You can use double quotes inside single quotes or vice versa to include quotes in your message.
+print('Hey there, welcome to your Python journey! Remember, "Practice makes perfect."')
+
+# Using single quotes instead of double quotes
+# Python allows you to use single quotes for strings as well. It works the same way as double quotes.
+print('Hey there, welcome to your Python journey!')
+
+
